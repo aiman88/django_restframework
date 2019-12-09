ed42b7a162558c6327cd808e699a993015417faa
aiman <paktikong@yahoo.ie>
2019-12-09 23:24:16 +0800

    ADding file

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aiman\PycharmProjects\django_restframework\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
